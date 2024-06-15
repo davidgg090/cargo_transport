b2c4fc8c9fa932efc7043bab6fcd8b1da0739993
David Gomez Garcia <davidgg090@gmail.com>
2024-06-14 23:41:34 -0400

    Add authentication routes
    The new code added to 'auth.py' provides two endpoints: one for user registration and another for token generation, i.e., user login. These endpoints utilize FastAPI's OAuth2PasswordRequestForm and SQLAlchemy models and sessions. They enable basic auth functionality in the application: user registration and login.

---
+++ app/routers/auth.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from datetime import timedelta
+from app.models import User, UserResponse, Token
+from app.auth.auth import register_user, login_for_access_token
+from app.dependencies import get_db
+
+router = APIRouter()
+
+
+@router.post("/register/", response_model=UserResponse)
+def register(user: User, db: Session = Depends(get_db)):
+    """
+    Registers a new user in the system.
+
+    Args:
+        user (User): The user data to register.
+        db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        dict: A dictionary containing the username of the newly registered user.
+    """
+
+    new_user = register_user(user, db)
+    return {"username": new_user.username}
+
+
+@router.post("/token/", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    """
+    Logs in a user to generate an access token.
+
+    Args:
+        form_data (OAuth2PasswordRequestForm, optional): The form data containing username and password. Defaults to Depends().
+        db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        Token: The generated access token.
+    """
+
+    return login_for_access_token(form_data, db)
