cb30f1a82a439a0a14b1c2ed482a5b7e69ac0ce4
David Gomez Garcia <davidgg090@gmail.com>
2024-06-15 00:52:13 -0400

    Set default database URL in SQLAlchemy
    The database URL in SQLAlchemy is updated so that in case the DATABASE_URL is not found in the environment variables, it will default to using sqlite with a local file named default.db. This ensures the application can still function without the need for explicit configuration.

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./default.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
